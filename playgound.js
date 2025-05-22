0ae90d602fc3be09c459c7403c28dd027a3cc624
sokol.matija <sokol.matija@gmail.com>
2025-05-22 13:15:11 +0200

    work

--- playgound.js
+++ playgound.js
@@ -3,3 +3,4 @@ function processArray(numbers){
 }
 
 console.log(processArray([1,2,3,4,5,6,7,8,9,10]));
+
---
+++ task02.js
@@ -0,0 +1,7 @@
+function analyzeText(text){
+    const lowerCaseText = text.toLowerCase();
+
+    const isPalindrome = str => {
+        const cleanedStr = str.replace(/[^a-z0-9/g,'']);
+    }
+}
